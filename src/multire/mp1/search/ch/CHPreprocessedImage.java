0a3e1c6d364c007df8ede5eea7224a3096761d6c
Earl <Earl@192.168.1.104>
2015-07-14 18:06:15 +0800

    added color histogram conversion

---
+++ src/multire/mp1/search/ch/CHPreprocessedImage.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multire.mp1.search.ch;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import multire.mp1.search.PreprocessedImage;
+import multire.mp1.provided.cieConvert;
+import java.awt.image.ColorModel;
+
+/**
+ *
+ * @author Earl
+ */
+public class CHPreprocessedImage extends PreprocessedImage{
+    int[] chLUV = new int[4];
+    
+    private void createCH(int index){
+        if(index >= 0 && index <= 39){ chLUV[0]++; }
+        else if(index >= 40 && index <= 79){ chLUV[1]++; }
+        else if(index >= 80 && index <= 119){ chLUV[2]++; }
+        else if(index >= 120 && index <= 158){ chLUV[3]++; }
+    }
+    
+    private void convertRGBToLUV(BufferedImage image){
+        ColorModel CM;
+        double answer=0, RGB;
+        double imageHeight, imageWidth;
+        double totalPixels;
+        double R, G, B;
+        int index;
+        cieConvert ColorCIE = new cieConvert();
+        
+        imageHeight = image.getHeight();
+        imageWidth = image.getWidth();
+        totalPixels = imageHeight * imageWidth;
+        CM = image.getColorModel();
+        
+        for(int x = 0; x < imageWidth; x++)
+            for(int y = 0; y < imageHeight; y++) {
+                RGB = image.getRGB(x,y);
+               
+                R = CM.getRed(RGB);
+                G = CM.getGreen(RGB);
+                B = CM.getBlue(RGB);
+                
+                ColorCIE.setValues(R/255.0, G/255.0, B/255.0);
+                index = ColorCIE.IndexOf();
+                createCH(index);
+            }           
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Starting!");
+        File imagefile = new File("/home/lugkhast/Desktop/calmdown.jpg");
+        BufferedImage srcImg;
+
+        try {
+            srcImg = ImageIO.read(imagefile);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(CHPreprocessedImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println("Done!");
+    }
+}
